9b1595e22dcaa433b4dfad261527cb871c6a2d9a
koonwen <koonwen@gmail.com>
2024-07-29 11:41:13 +0200

    Blogpost benchmarks

--- examples/blogpost/bench.sh
+++ examples/blogpost/bench.sh
@@ -8,61 +8,62 @@ rm -rf $SRC $DST
 
 # Setup FS & build cp.exe
 dune build -- ./cp.exe
-dune exec -- ../fs_gen.exe 5 5 . $SRC 4096
+dune exec -- ../fs_gen.exe 5 7 . $SRC 4096
 
-# Workload 1
+# Benchmark 1
 hyperfine --warmup 5 \
-          --prepare "rm -rf $DST" \
-          "cp -R $SRC $DST"
-
-hyperfine --warmup 5 \
-          --prepare "rm -rf $DST" \
-          "$EIO_CP $SRC $DST 4096"
-
-
-# Remake FS with 1Mb files
-rm -rf $SRC $DST
-dune exec -- ../fs_gen.exe 5 4 . $SRC 1000000
-
-# Workload 2
-hyperfine --warmup 5 \
-          --prepare "rm -rf $DST" \
-          "cp -R $SRC $DST"
+          --prepare "rm -rf $DST; sync" \
+	  --export-json cp_r_4k.json \
+          "cp -r $SRC $DST"
 
 hyperfine --warmup 5 \
-          --prepare "rm -rf $DST" \
-          "$EIO_CP $SRC $DST 4096"
+          --prepare "rm -rf $DST; sync" \
+	  --export-json eio_cp_4k.json \
+          "$EIO_CP $SRC $DST"
 
 hyperfine --warmup 5 \
-	  --prepare "rm -rf $DST" \
-          "$EIO_CP $SRC $DST 1000000"
-
-# With a cold cache
-echo "Testing with cold caches"
-
-hyperfine --warmup 5 \
-          --setup "sudo -v" \
-          --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
-          "cp -R $SRC $DST"
+	  --setup "sudo -v" \
+	  --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
+	  --export-json cp_r_4k_fsync+cold.json \
+          "cp -r $SRC $DST; sync"
 
 hyperfine --warmup 5 \
-          --setup "sudo -v" \
-          --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
-          --parameter-list blksz 4096,1000000 \
-          "$EIO_CP $SRC $DST {blksz}"
+	  --setup "sudo -v" \
+	  --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
+	  --export-json eio_cp_4k_fsync+cold.json \
+          "$EIO_CP $SRC $DST; sync"
 
-rm -rf $SRC $DST
+# # Remake FS with 1Mb files
+# rm -rf $SRC $DST
+# dune exec -- ../fs_gen.exe 5 4 . $SRC 1000000
 
-# Setup FS & build cp.exe
-dune build -- ./cp.exe
-dune exec -- ../fs_gen.exe 5 5 . $SRC 4096
+# # Benchmark 2
+# hyperfine --warmup 5 \
+#           --prepare "rm -rf $DST; sync" \
+# 	  --export-json cp_r_1mb.json \
+# 	  --show-output \
+#           "cp -R $SRC $DST"
 
-hyperfine --warmup 5 \
-          --setup "sudo -v" \
-          --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
-          "cp -R $SRC $DST"
+# hyperfine --warmup 5 \
+#           --prepare "rm -rf $DST; sync" \
+# 	  --export-json eio_cp_1mb.json \
+#           "$EIO_CP $SRC $DST"
 
-hyperfine --warmup 5 \
-          --setup "sudo -v" \
-          --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
-          "$EIO_CP $SRC $DST 4096"
+# hyperfine --warmup 5 \
+# 	  --setup "sudo -v" \
+# 	  --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
+# 	  --export-json cp_r_1mb_fsync+cold.json \
+#           "cp -r $SRC $DST; sync"
+
+# hyperfine --warmup 5 \
+# 	  --setup "sudo -v" \
+# 	  --prepare "rm -rf $DST; sync; echo 3 | sudo tee /proc/sys/vm/drop_caches" \
+# 	  --export-json eio_cp_1mb_fsync+cold.json \
+#           "$EIO_CP $SRC $DST; sync"
+
+# # Benchmark 3
+# hyperfine --warmup 5 \
+# 	  --prepare "rm -rf $DST; sync" \
+# 	  --export-json eio_cp_1mb_var_blksz.json \
+# 	  --parameter-list blksz 4096,16384,65536,262144,1048576 \
+#           "$EIO_CP $SRC $DST {blksz}"
--- examples/blogpost/cp.ml
+++ examples/blogpost/cp.ml
@@ -1,3 +1,4 @@
+[@@@warning "-32-26"]
 open Eio
 
 let ( / ) = Eio.Path.( / )
@@ -5,7 +6,7 @@ let ( / ) = Eio.Path.( / )
 let copy_dfs concurrent src dst =
   let rec aux ~src ~dst =
     let stat = Path.stat ~follow:false src in
-    match stat.kind with
+    (match stat.kind with
     | `Directory ->
       Path.mkdir ~perm:stat.perm dst;
       let files = Path.read_dir src in
@@ -15,9 +16,45 @@ let copy_dfs concurrent src dst =
       Path.with_open_in src @@ fun source ->
       Path.with_open_out ~create:(`Exclusive stat.perm) dst @@ fun sink ->
       Flow.copy source sink;
-    | _ -> failwith "Not sure how to handle kind"
+    | _ -> failwith "Not sure how to handle kind");
   in
   aux ~src ~dst
+
+module Q = Eio_utils.Lf_queue
+
+let copy_bfs src dst =
+
+  let sem = Semaphore.make 64 in
+  let q = Q.create () in
+  Q.push q (src, dst);
+
+  Switch.run @@ fun sw ->
+  while not (Q.is_empty q) do
+
+    match Q.pop q with
+    | None -> failwith "None in queue"
+    | Some (src_path, dst_path) ->
+      begin
+        let stat = Path.stat ~follow:false src_path in
+        (match stat.kind with
+         | `Directory ->
+           Path.mkdir ~perm:stat.perm dst_path;
+           let files = Path.read_dir src_path in
+           (* Append files in found directory *)
+           List.iter (fun f -> Q.push q (src_path / f, dst_path / f)) files
+         | `Regular_file ->
+           Fiber.fork ~sw (fun () ->
+               Semaphore.acquire sem;
+               Path.with_open_in src_path @@ fun source ->
+               Path.with_open_out ~create:(`Exclusive stat.perm) dst_path @@ fun sink ->
+               Flow.copy source sink;
+               Semaphore.release sem
+             )
+         | _ -> failwith "Not sure how to handle kind");
+        end
+
+  done
+
 
 let () =
   let block_size = try Some (int_of_string Sys.argv.(3)) with _ -> None in
@@ -26,4 +63,4 @@ let () =
       let cwd = Eio.Stdenv.cwd env in
       let src = cwd / Sys.argv.(1) in
       let dst = cwd / Sys.argv.(2) in
-      copy_dfs concurrent src dst)
+      copy_bfs src dst)
