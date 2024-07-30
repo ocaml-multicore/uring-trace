3f34ad5439688f2cf676eee8a6811d71e32e4444
koonwen <koonwen@gmail.com>
2024-07-30 18:03:48 +0200

    Revert "add sites logic into build"
    This reverts commit 237fac821ba34753b15caf681b4d1cc33f5c3601.

--- src/main.ml
+++ src/main.ml
@@ -278,7 +278,33 @@ let handle_event (writer : W.t) _ctx data _size =
   0
 
 let () =
-  run ~bpf_object_path:Site.bpf_object_path
-    ~bpf_program_names:Site.bpf_program_names
+  run ~bpf_object_path:"uring.bpf.o"
+    ~bpf_program_names:
+      [
+        "handle_create";
+        "handle_register";
+        "handle_file_get";
+        "handle_submit_req";
+        "handle_queue_async_work";
+        "handle_poll_arm";
+        "handle_task_add";
+        "handle_task_work_run";
+        "handle_short_write";
+        "handle_local_work_run";
+        "handle_defer";
+        "handle_link";
+        "handle_fail_link";
+        "handle_cqring_wait";
+        "handle_req_failed";
+        "handle_cqe_overflow";
+        "handle_complete";
+        "handle_io_init_new_worker";
+        "handle_sys_enter_io_uring_setup";
+        "handle_sys_exit_io_uring_setup";
+        "handle_sys_enter_io_uring_register";
+        "handle_sys_exit_io_uring_register";
+        "handle_sys_enter_io_uring_enter";
+        "handle_sys_exit_io_uring_enter";
+      ]
     handle_event;
   Printf.printf "User space consumed %d events" !cb
